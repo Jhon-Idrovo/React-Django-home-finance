bc66334a2796457e922ef246acb8cd68e09b1acd
Jhon-Idrovo <Jhon_Idrovo3@users.noreply.github.com>
2021-03-20 21:19:58 -0500

    working

--- statistics_API/serializers.py
+++ statistics_API/serializers.py
@@ -4,4 +4,4 @@ from expenses.models import Expense
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
-        fields = '__all__'
+        exclude = ['user']
